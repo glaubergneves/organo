b142ba534ea163dec74700f0cd4ed9454507ef08
Glauber Neves <glaubergneves@gmail.com>
2024-02-28 11:10:07 -0300

    cria projeto organo

---
+++ src/components/Formulario/index.js
@@ -0,0 +1,60 @@
+import "./Formulario.css";
+import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
+import Botao from "../Botao";
+import { useState } from "react";
+
+const Formulario = (props) => {
+  const [nome, setNome] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    props.aoColaboradorCadastrado({
+      nome,
+      cargo,
+      imagem,
+      time
+    });
+  };
+
+  return (
+    <section className="formulario">
+      <form onSubmit={aoSalvar}>
+        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <CampoTexto
+          value={nome}
+          aoAlterado={(valor) => setNome(valor)}
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite o seu nome"
+        />
+        <CampoTexto
+          value={cargo}
+          aoAlterado={(valor) => setCargo(valor)}
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite o seu cargo"
+        />
+        <CampoTexto
+          value={imagem}
+          aoAlterado={(valor) => setImagem(valor)}
+          label="Imagem"
+          placeholder="Digite o endereço da imagem"
+        />
+        <ListaSuspensa
+          valor={time}
+          aoAlterado={valor => setTime(valor)}
+          required={true}
+          label="Time"
+          itens={props.times}
+        />
+        <Botao>Criar Card</Botao>
+      </form>
+    </section>
+  );
+};
+
+export default Formulario;
